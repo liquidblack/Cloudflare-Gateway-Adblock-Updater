6fb6e958fd9518f89712b6e9ead1e6704660b2fe
Samuel <liquidblack@users.noreply.github.com>
2026-09-14 17:00:45 +0200

    Update blocklist name for Hagezi Pro

--- update_gateway.py
+++ update_gateway.py
@@ -58,7 +58,7 @@ session.headers.update(headers)
 # 10000+: Hagezi filters (ordered by importance)
 blocklists: List[Dict[str, str]] = [
     {
-        "name": "Hagezi Pro",
+        "name": "Hagezi Pro++",
         "url": "https://hagezi-mirror.dnsbunker.org/wildcard/pro-onlydomains.txt",
         "backup_url1": "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro-onlydomains.txt",
         "backup_url2": "https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/wildcard/pro-onlydomains.txt",
